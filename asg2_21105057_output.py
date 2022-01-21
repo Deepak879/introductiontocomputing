2482b4ca368a2d78c8fe9bfc2c7a9156705d1c9d
Deepak879 <97351034+Deepak879@users.noreply.github.com>
2022-01-21 16:39:03 +0530

    Add files via upload

---
+++ asg2_21105057_output.py
@@ -0,0 +1,43 @@
+Python 3.9.8 (tags/v3.9.8:bb3fdcf, Nov  5 2021, 20:33:01) [MSC v.1929 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: C:\Users\User\Desktop\asg2_21105057.py ===============
+#program 1
+Enter the string : Python is a case sensitive language
+A. Length of the entered string :  35
+B. Revere of the string:  egaugnal evitisnes esac a si nohtyP
+C. Sliced string :  a case sensitive language
+D.String after replacement :  Python is object oriented language
+E.Index of 'a' in the string :  10
+F.removal of blank whitespaces with empty string :  Pythonisacasesensitivelanguage
+
+ #program 2
+Hey, 
+ DEEPAK KALYAN Here!.
+My SID is 21105057 
+I am from ECE Department and My CGPA is 9.9
+
+ #program 3
+a. a&b=   8
+b. a|b=   58
+c. a^b=   50
+d. Left shift both a and b with 2 bits : (224, 40)
+e. Right shift a with 2 bits and b with 4 bits:  (14, 0)
+
+ #program 4
+Enter first number :87
+Enter second number :86
+Enter third number :85
+highest_number =  87
+
+ #program 5
+Enter input string: Your name
+Yes
+
+ #program 6
+Length of first side = 5
+Length of second side = 7
+Length of third side = 9
+Yes,Trangle can be made.
+
+>>> 
